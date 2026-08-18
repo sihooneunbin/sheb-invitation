ab203ca218df37695ad971cf80f6013ec51e598a
sihooneunbin <eunbin1231@gmail.com>
2026-08-18 16:29:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const musicButton = document.getElementById("musicButton");
+const bgMusic = document.getElementById("bgMusic");
 
+if (musicButton && bgMusic) {
+  musicButton.addEventListener("click", () => {
+    if (bgMusic.paused) {
+      bgMusic.play();
+      musicButton.classList.add("playing");
+    } else {
+      bgMusic.pause();
+      musicButton.classList.remove("playing");
+    }
+  });
+}
